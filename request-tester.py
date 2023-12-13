b7cc34c31fb46c3f218540d5d73b8004bf0d6d71
jchakir <105322451+jchakir@users.noreply.github.com>
2023-12-13 09:13:34 +0100

    add request-tester.py: input images as args

--- request-tester.py
+++ request-tester.py
@@ -1,15 +1,17 @@
 import requests
 import json
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
 
 url = 'http://localhost:8501/v1/models/faceid:predict'
+if len(sys.argv) != 3:
+  print(f"arguments error: expected {sys.argv[0] im1_path.png im2_path.png }")
 
-# im1 = plt.imread('./small_dataset/100000/0.png')[:, :, :3]
-# im2 = plt.imread('./small_dataset/100000/0.png')[:, :, :3]
+im1_path, im2_path = sys.argv[1], sys.argv[2]
 
-im1 = np.random.random((112, 112, 3))
-im2 = np.random.random((112, 112, 3))
+im1 = plt.imread(im1_path)[:, :, :3]
+im2 = plt.imread(im2_path)[:, :, :3]
 
 data = json.dumps({ "instances": [{
         "anchor_img": im1.tolist() ,
@@ -19,7 +21,4 @@ data = json.dumps({ "instances": [{
 
 response = requests.post(url, data=data, headers={ "content-type": "application/json" })
 
-prediction = json.loads(response.text)
-
-print(prediction)
-
+print(response.text)
